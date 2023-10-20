15d746795e9d72581de03540011f972ec9f3275b
Joao Passos <joao.passos@invillia.com>
2023-10-20 00:03:55 -0300

    ajustes rota

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable, tap } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth';
+  private apiUrl = 'https://drink-event-release.vercel.app/api/auth';
 
   constructor(private http: HttpClient) {}
 
